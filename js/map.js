b4ffcfa1c50191532c6d98f7bd155d219344d125
Sarazar928Ghost <van.wassenhove.kevin@hotmail.com>
2024-05-01 15:40:55 +0200

    target blank github link ti3_2024

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var marker = L.marker([50.8256084, 4.3385623]).addTo(map);
 
-const presentation = "<p><strong>TI3_2024</strong></p><p>Site de connexion/deconnexion avec une gestion des points de localisations sur la carte.</p><p style='display:flex; justify-content: space-around;'><a href='https://github.com/OnlyPassionCode/TI3-2024'>Lien Github</a><a href='ti3-2024'>Lien vers le site</a></p>"
+const presentation = "<p><strong>TI3_2024</strong></p><p>Site de connexion/deconnexion avec une gestion des points de localisations sur la carte.</p><p style='display:flex; justify-content: space-around;'><a href='https://github.com/OnlyPassionCode/TI3-2024' target='_blank'>Lien Github</a><a href='ti3-2024'>Lien vers le site</a></p>"
 
 
 marker.bindPopup(presentation).openPopup();
